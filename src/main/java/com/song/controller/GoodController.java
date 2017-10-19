b9fd73f5b5047837a254f793aaf2ed76c70a9f46
songxiaohui <songyh_1314@163.com>
2017-10-19 17:07:33 +0800

    ssm 框架

--- src/main/java/com/song/controller/GoodController.java
+++ src/main/java/com/song/controller/GoodController.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
@@ -28,6 +30,12 @@ public class GoodController {
         return maps;
     }
 
-
+    @RequestMapping("/uploadGood")
+    public void uploadGood(@RequestParam String goodName,double goodPrice,String goodDesc,
+                           @RequestParam("photoFile")MultipartFile file) throws Exception{
+        System.out.println("--------");
+        System.out.println(goodName);
+        System.out.println(file.getOriginalFilename());
+    }
 
 }
